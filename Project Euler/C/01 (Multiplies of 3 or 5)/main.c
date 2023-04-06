cd98cd47b903e5ad156ee24d729138066b5ca846
Piotr Marendowski <piotr-marendowski@tutanota.com>
2023-04-06 13:45:11 +0200

    Add explanation

--- Project Euler/C/01 (Multiplies of 3 or 5)/main.c
+++ Project Euler/C/01 (Multiplies of 3 or 5)/main.c
@@ -10,10 +10,10 @@ int main(void)
 
 	for (; num < 1000; num++)
 	{
+		/* if not modulo means if the value of this expression
+		is NOT a non-zero = IS ZERO */
 		if (!(num % 3) || !(num % 5))
-		{
 			sum += num;
-		}
 	}
 
 	printf("Sum: %d\n", sum);
